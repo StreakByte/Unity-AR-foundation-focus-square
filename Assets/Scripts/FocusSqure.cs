4065f0f27fe1cf8dd7abb5d477642918bea1fbd3
Digvijaysinh Gohil <info@a1genious.com>
2020-06-10 12:06:35 +0530

    InitialCommit

---
+++ Assets/Scripts/FocusSqure.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class FocusSqure : MonoBehaviour
+{
+    private ARRaycastManager arRaycastManager;
+    private Transform cameraTransform;
+    private Vector2 centerScreen;
+    private Vector3 initialScale;
+    
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    [SerializeField] private ARCameraManager arCameraManager;
+    [Header("Focus Square")]
+    [SerializeField] private GameObject focusSquare;
+    [Header("Model To Augment")]
+    [SerializeField] private GameObject modal;
+
+    private void Awake()
+    {
+        modal.SetActive(false);
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        cameraTransform = Camera.main.transform;
+        centerScreen = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        initialScale = focusSquare.transform.localScale;
+    }
+
+    private void Update()
+    {
+        HandleFocusSquare();
+    }
+
+    private void HandleFocusSquare()
+    {
+        if (arRaycastManager.Raycast(centerScreen, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            float distance = Vector3.Distance(cameraTransform.position, focusSquare.transform.position);
+ 
+            Pose hitPose = hits[0].pose;
+            focusSquare.transform.position = hitPose.position;
+            focusSquare.transform.rotation = hitPose.rotation;
+            focusSquare.transform.localScale = initialScale * distance;
+
+            // Handle taps here
+            if (Input.GetMouseButtonDown(0))
+            {
+                modal.SetActive(true);
+                modal.transform.position = hitPose.position;
+                modal.transform.rotation = hitPose.rotation;
+                // Compansate Modal Rotation to face the Camera
+                modal.transform.Rotate(modal.transform.rotation.x, modal.transform.rotation.y + 180f, modal.transform.rotation.z);
+                focusSquare.SetActive(false);
+            }
+        }
+    }    
+}
---
+++ Assets/_IMPORTED/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+/// <summary>
+/// Listens for touch events and performs an AR raycast from the screen touch point.
+/// AR raycasts will only hit detected trackables like feature points and planes.
+///
+/// If a raycast hits a trackable, the <see cref="placedPrefab"/> is instantiated
+/// and moved to the hit position.
+/// </summary>
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_PlacedPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedPrefab
+    {
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject1 { get; private set; }
+    public GameObject spawnedObject2 { get; private set; }
+
+    private bool toggle = false;
+
+    private Transform firstSphere;
+    private Transform secondSphere;
+
+    public GameObject spherePrefab;
+    public LineRenderer lineRenderer;
+    public Text distanceText;
+    public Vector3 offset;
+    
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            var mousePosition = Input.mousePosition;
+            touchPosition = new Vector2(mousePosition.x, mousePosition.y);
+            return true;
+        }
+#else
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+#endif
+
+        touchPosition = default;
+        return false;
+    }
+
+    void Update()
+    {
+        if (toggle && secondSphere != null)
+        {
+            distanceText.text = System.Math.Round((Vector3.Distance(firstSphere.position, secondSphere.position) * 100f), 2) + " cm";
+            lineRenderer.SetPosition(0, firstSphere.position);
+            lineRenderer.SetPosition(1, secondSphere.position);
+        }
+
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = s_Hits[0].pose;
+
+            if(!toggle)
+            {
+                if (spawnedObject1 == null)
+                {
+                    spawnedObject1 = Instantiate(spherePrefab, hitPose.position, hitPose.rotation);
+                    firstSphere = spawnedObject1.transform;
+                }
+                //else
+                //{
+                //    spawnedObject1.transform.position = hitPose.position;
+                //}
+            }
+            else if (toggle)
+            {
+                if (spawnedObject2 == null)
+                {
+                    spawnedObject2 = Instantiate(spherePrefab, hitPose.position, hitPose.rotation);
+                    secondSphere = spawnedObject2.transform;
+                }
+                else
+                {
+                    spawnedObject2.transform.position = hitPose.position;
+                }
+            }
+        }
+
+        
+    }
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    ARRaycastManager m_RaycastManager;
+
+    public void OnToggleButtonClicked()
+    {
+        toggle = !toggle;
+    }
+}
